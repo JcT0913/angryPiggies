1e269f00d317a20263facc8b46d8b8901f242c22
JcT0913 <tjc9606ok@gmail.com>
2022-02-09 13:28:21 -0500

    move scripts to folder

---
+++ angryPiggies - Jiachen Tang/Assets/_Scripts/scoreCounter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreCounter : MonoBehaviour
+{
+    public float scoreCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
